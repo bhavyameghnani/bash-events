314a5fcbaa038bb3cea9fb235003b240b06ece76
bhavyameghnani <meghnani.bhavya@gmail.com>
2022-04-19 20:07:33 +0530

    Route fixed for dynamic pages

--- ui/src/Components/HomePage/EventCatergoryPage.js
+++ ui/src/Components/HomePage/EventCatergoryPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from "react-router";
 
 function EventCategoryPage(){
+    const { cid } = useParams();
+    let id = parseInt(cid);
     return(
         <div>
             dynamic event page for all 6 categories
